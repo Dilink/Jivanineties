769c48f1031e18220f9af9a0a66802c92a5b7996
Dilink <dilink4@gmail.com>
2020-04-27 20:20:56 +0200

    Add absorption test

---
+++ Assets/Scripts/AbsorptionArea.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class AbsorptionArea : MonoBehaviour
+{
+    public AbsorptionMaterialChange materials;
+
+    private MeshRenderer meshRenderer;
+
+    private void Start()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+        meshRenderer.material = materials.materialBefore;
+    }
+
+    public void OnAbsorption()
+    {
+        meshRenderer.material = materials.materialAfter;
+    }
+}
---
+++ Assets/Scripts/AbsorptionController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class AbsorptionController : MonoBehaviour
+{
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			int layerMask = 1 << LayerMask.NameToLayer("Player");
+			layerMask = ~layerMask;
+
+			RaycastHit hit;
+			if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.down), out hit, 1.0f, layerMask))
+			{
+				var area = hit.transform.gameObject.GetComponent<AbsorptionArea>();
+				if (area)
+				{
+					area.OnAbsorption();
+				}
+			}
+		}
+	}
+}
