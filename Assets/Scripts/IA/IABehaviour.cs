b275560a13b64a5d6df44f3f9b1b3d92d28b6b26
LHouyezGlobalsoul <houyez.louis.lille@gmail.com>
2020-04-27 18:35:14 +0200

    IA folder

---
+++ Assets/Scripts/IA/IABehaviour.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(IAAttackGestion))]
+public class IABehaviour : MonoBehaviour
+{
+    public GameObject issou;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
