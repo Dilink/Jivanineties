98b44a991d55e477a143886a13416385ffe0baf2
Dilink <dilink4@gmail.com>
2020-04-29 01:25:32 +0200

    Add absorption area mesh creation

--- Assets/Scripts/AbsorptionArea.cs
+++ Assets/Scripts/AbsorptionArea.cs
@@ -1,10 +1,22 @@
-﻿using UnityEngine;
+﻿using Sirenix.OdinInspector;
+using UnityEditor;
+using UnityEngine;
 
 public class AbsorptionArea : MonoBehaviour, IAbsorbable
 {
     public AbsorptionMaterialChange materials;
 
+    [OnValueChanged("OnBoundariesChanged")]
+    [ShowIf("isEditingBoundaries")]
+    public Vector3[] boundaries;
+
+    [HideInInspector]
+    public bool isEditingBoundaries;
+
+    private int lastBoundariesLength = 0;
     private MeshRenderer meshRenderer;
+
+    private static readonly System.Random random = new System.Random();
 
     private void Start()
     {
@@ -33,4 +45,94 @@ public class AbsorptionArea : MonoBehaviour, IAbsorbable
         meshRenderer.material = materials.materialBefore;
         return true;
     }
+
+#if UNITY_EDITOR
+    private Vector3 GetRandomPointAround(Vector3 center, float radius)
+    {
+        float angle = (float)(2.0 * System.Math.PI * random.NextDouble());
+
+        float randX = center.x + radius * Mathf.Cos(angle);
+        float randZ = center.z + radius * Mathf.Sin(angle);
+
+        return new Vector3(randX, center.y, randZ);
+    }
+
+    private void OnBoundariesChanged()
+    {
+        if (boundaries.Length > lastBoundariesLength)
+        {
+            float radius = Random.Range(0.1f, Mathf.Min(transform.localScale.x, transform.localScale.z) / 2.0f);
+            boundaries[boundaries.Length - 1] = GetRandomPointAround(transform.position, radius);
+        }
+        lastBoundariesLength = boundaries.Length;
+    }
+
+    private Mesh CreateMesh(Vector2[] vertices2d, int[] indices)
+    {
+        Vector3[] vertices = new Vector3[vertices2d.Length];
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            vertices[i] = new Vector3(transform.position.x - vertices2d[i].x, transform.position.y, transform.position.z - vertices2d[i].y);
+        }
+        
+        Mesh mesh = new Mesh();
+        mesh.vertices = vertices;
+        mesh.triangles = indices;
+        mesh.RecalculateNormals();
+
+        string filePath = EditorUtility.SaveFilePanelInProject("Save Procedural Mesh", "Procedural Mesh", "asset", "");
+        if (filePath != "")
+        {
+            AssetDatabase.CreateAsset(mesh, filePath);
+        }
+
+        return mesh;
+    }
+
+    [Button(ButtonSizes.Medium), GUIColor(0.58f, 0.58f, 0.58f)]
+    [HideIf("isEditingBoundaries")]
+    public void EditBoundaries()
+    {
+        isEditingBoundaries = true;
+    }
+
+    [HorizontalGroup("Split", 0.5f)]
+    [Button(ButtonSizes.Medium), GUIColor(0.14f, 0.89f, 0.14f)]
+    [ShowIf("isEditingBoundaries")]
+    public void BakeBoundaries()
+    {
+        isEditingBoundaries = false;
+
+        int[] sourceIndices;
+
+        Vector2[] sourceVertices = new Vector2[boundaries.Length];
+        for (int i = 0; i < boundaries.Length; i++)
+            sourceVertices[i] = new Vector2(boundaries[i].x, boundaries[i].z);
+
+        if (sourceVertices.Length > 0)
+        {
+            Triangulator2D.Triangulator.Triangulate(sourceVertices, Triangulator2D.WindingOrder.Clockwise, out sourceVertices, out sourceIndices);
+        }
+        else
+        {
+            sourceIndices = new int[sourceVertices.Length / 3];
+        }
+        
+        Mesh mesh = CreateMesh(sourceVertices, sourceIndices);
+
+        MeshFilter meshFilter = GetComponent<MeshFilter>();
+        MeshCollider meshCollider = GetComponent<MeshCollider>();
+
+        meshFilter.mesh = mesh;
+        meshCollider.sharedMesh = mesh;
+    }
+
+    [HorizontalGroup("Split", 0.5f)]
+    [Button(ButtonSizes.Medium, Name = "Cancel"), GUIColor(0.89f, 0.14f, 0.14f)]
+    [ShowIf("isEditingBoundaries")]
+    public void CancelBakeBoundaries()
+    {
+        isEditingBoundaries = false;
+    }
+#endif
 }
---
+++ Assets/Scripts/Editor/AbsorptionAreaBoundaries.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sirenix.OdinInspector.Editor;
+using UnityEditor;
+
+[CustomEditor(typeof(AbsorptionArea))]
+public class AbsorptionAreaBoundaries : OdinEditor
+{
+    private AbsorptionArea area;
+
+    new void OnEnable()
+    {
+        base.OnEnable();
+        area = target as AbsorptionArea;
+    }
+
+    void OnSceneGUI()
+    {
+        if (!area.isEditingBoundaries)
+        {
+            return;
+        }
+
+        for (int i = 0; i < area.boundaries.Length; i++)
+        {
+            Handles.Label(area.boundaries[i] + Vector3.up * 1.5f, "" + (i + 1));
+
+            EditorGUI.BeginChangeCheck();
+
+            Vector3 newPosition = Handles.PositionHandle(area.boundaries[i], Quaternion.identity);
+
+            if (EditorGUI.EndChangeCheck())
+            {
+                area.boundaries[i] = newPosition;
+            }
+
+            if (area.boundaries.Length > 1)
+            {
+                Handles.DrawPolyLine(area.boundaries);
+                Handles.DrawPolyLine(area.boundaries[0], area.boundaries[area.boundaries.Length - 1]);
+            }
+        }
+    }
+}
