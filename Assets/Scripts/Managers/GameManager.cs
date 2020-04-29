e64be09649f25dffb74e96ed50872610952fcbc2
Dilink <dilink4@gmail.com>
2020-04-29 11:31:40 +0200

    Add GameManager and UIManager

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class GameManager : Singleton<GameManager>
+{
+    /// <summary>
+    /// Editor only
+    /// </summary>
+    public int tokendoCount;
+
+    private UIManager uiManager;
+
+    /// <summary>
+    /// Gameplay only
+    /// </summary>
+    private int _tokendoAmount = 0;
+    private int tokendoAmount
+    {
+        get => _tokendoAmount;
+        set
+        {
+            uiManager.UpdateTokendoCount(_tokendoAmount);
+        }
+    }
+
+    void Start()
+    {
+        _tokendoAmount = tokendoCount;
+        uiManager = GetComponent<UIManager>();
+    }
+
+    public void AddTokendo(int count)
+    {
+        tokendoAmount += count;
+    }
+}
