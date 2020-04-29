3e4c940eb5af518d6a786ee2ddc776a2b3d91d18
Dilink <dilink4@gmail.com>
2020-04-29 15:29:34 +0200

    Set InputManager reference inside GameManager

--- Assets/Scripts/Managers/InputManager.cs
+++ Assets/Scripts/Managers/InputManager.cs
@@ -1,12 +1,7 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class InputManager: MonoBehaviour
 {
-
-    public static InputManager Instance;
-
     public bool UP { get { return Input.GetAxis("Vertical") > 0; } }
     public bool DOWN { get { return Input.GetAxis("Vertical") < 0; } }
     public bool RIGHT { get { return Input.GetAxis("Horizontal") > 0; } }
@@ -17,28 +12,4 @@ public class InputManager: MonoBehaviour
     public bool ATTACK_HOLD { get { return Input.GetButton("Attack"); } }
     public bool DODGE_HOLD { get { return Input.GetButton("Dodge"); } }
     public bool POWER_HOLD { get { return Input.GetButton("Power"); } }
-
-    private void Awake()
-    {
-        if(InputManager.Instance == null)
-        {
-            InputManager.Instance = this;
-        }
-        else
-        {
-            Destroy(this);
-        }
-    }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
 }
