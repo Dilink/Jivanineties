6f1b2681a21530196d2a6460a53c46dba8fbd5a4
Dilink <dilink4@gmail.com>
2020-04-29 14:53:20 +0200

    Add phase to wait for all enemies to be dead

---
+++ Assets/Scripts/CombatPhases/WaitForAllEnemiesDeath.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+
+[System.Serializable]
+public class WaitForAllEnemiesDeath : ICombatPhase
+{
+    public IEnumerator Execute(Action onEnd, CombatPhaseData data)
+    {
+        while (GameManager.Instance.remainingEnemies.Count > 0)
+        {
+            yield return null;
+        }
+    }
+}
