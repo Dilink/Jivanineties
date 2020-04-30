026066854902be858aaf3cfc487f19387f39eeef
Dilink <dilink4@gmail.com>
2020-04-30 23:07:04 +0200

    Add phase to modify tokendo amount

---
+++ Assets/Scripts/CombatPhases/TokendoPhase.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+[System.Serializable]
+public class TokendoPhase : ICombatPhase
+{
+	public int amount;
+
+	public IEnumerator Execute(Action onEnd, CombatPhaseData data)
+	{
+		GameManager.Instance.tokendoAmount += amount;
+
+		onEnd();
+
+		yield return null;
+	}
+
+	public CombatPhaseType ReturnType()
+	{
+		return CombatPhaseType.WaitPhase;
+	}
+}
