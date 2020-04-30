413c84df6cfb96125b177d8ddcbae57bba1e0a90
LHouyezGlobalsoul <houyez.louis.lille@gmail.com>
2020-04-30 20:46:42 +0200

    Sound Manager

--- Assets/Scripts/GameSound.cs
+++ Assets/Scripts/GameSound.cs
@@ -7,4 +7,26 @@ public enum GameSound
     // It's advisable to keep 'None' as the first option, since it helps exposing this enum in the Inspector.
     // If the first option is already an actual value, then there is no "nothing selected" option.
     None,
+
+    Payer_Attack,
+    Payer_Attack_Up,
+    Payer_Dash,
+    Payer_Dash_Up,
+    Payer_Death,
+    Payer_TakeDamage,
+
+    WaterShied,
+    WaterIn,
+    WaterOout,
+
+    Pickup_Tokkendo,
+
+    EnemyAttack,
+    EnemyAttack_up,
+    Enemy_stun,
+    Enemy_noise,
+    Enemy_takeDamage,
+    Enemy_Death,
+    Death_Smoke,
+
 }
